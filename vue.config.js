1ed759bfcd6da10d6152ea2c15681cc102af2581
Hehe00052 <phuonghtnbc00052@fpt.edu.vn>
2025-04-06 21:30:10 +0700

    edit: movement

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/berio/' // Đảm bảo có dấu / ở cả đầu và cuối
+    : '/'
 })
