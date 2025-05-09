c5f7b747402d463554d0bfe218177a12818cc565
Huynh Tran Nam Phuong <133868667+Hehe00052@users.noreply.github.com>
2025-05-09 14:45:54 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: false,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/berio/' // Đảm bảo có dấu / ở cả đầu và cuối
-    : '/'
+    : '/',
+  devServer: {
+    https: true,
+    host: '0.0.0.0',
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: {
+        protocol: 'wss',
+        hostname: 'urban-guacamole-r4r6p65667jhpwj9-8080.app.github.dev',
+        port: 443,
+        pathname: '/ws'
+      }
+    }
+  }
 })
